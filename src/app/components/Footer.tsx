cdc662ea83f927f9083b2271f6a015ad11ea86ef
Kevin Alejandro Murillo Salazar <71883283+KevinTheHacker10@users.noreply.github.com>
2024-05-27 02:25:09 -0600

    Update Footer.tsx
    Error fix on footer 
    
    Again

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FC = () => {
         <a href="https://www.instagram.com/kevinthehackerxd/" target="_blank" rel="noopener noreferrer">
         <svg xmlns="http://www.w3.org/2000/svg"  width="3em" height="3em" viewBox="0 0 15 15"><path fill="none" stroke="currentColor" d="M11 3.5h1M4.5.5h6a4 4 0 0 1 4 4v6a4 4 0 0 1-4 4h-6a4 4 0 0 1-4-4v-6a4 4 0 0 1 4-4Zm3 10a3 3 0 1 1 0-6a3 3 0 0 1 0 6Z"/></svg>
         </a>
-        <a href="www.linkedin.com/in/KevinTheHacker" target="_blank" rel="noopener noreferrer">
+        <a href="www.linkedin.com/in/kevinthehacker" target="_blank" rel="noopener noreferrer">
         <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"/></svg>
         </a>
       </div>
